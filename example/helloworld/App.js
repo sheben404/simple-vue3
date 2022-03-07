62124302491199a1220cd3708223cf6ea451a369
sheben404 <sheben404@gmail.com>
2022-03-07 10:42:15 +0800

    feat: 实现 element 初始化主流程

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -3,7 +3,18 @@ import { h } from "../../lib/mini-vue.esm.js";
 export const App = {
   render() {
     // ui
-    return h("div", "hi, " + this.msg);
+    return h(
+      "div",
+      {
+        id: "root",
+        class: ["red", "soft"],
+      },
+      // "hi, " + this.msg
+      // string
+      // "hi, mini-vue！"
+      // array
+      [h("p", { class: "red" }, "hi"), h("p", { class: "blue" }, "mini-vue")]
+    );
   },
   setup() {
     return {
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,11 +1,42 @@
+import { isObject } from "../shared/index";
 import { createComponentInstance, setupComponent } from "./component";
 
 export function render(vnode, container) {
   patch(vnode, container);
 }
 function patch(vnode, container) {
-  // 处理组件
-  processComponent(vnode, container);
+  debugger
+  if (typeof vnode.type === "string") {
+    processElement(vnode, container);
+  } else if (isObject(vnode.type)) {
+    // 处理组件
+    processComponent(vnode, container);
+  }
+}
+
+function processElement(vnode, container) {
+  mountElement(vnode, container);
+}
+
+function mountElement(vnode, container) {
+  const { type, props, children } = vnode;
+  const el = document.createElement(type);
+  for (const key in props) {
+    const val = props[key];
+    el.setAttribute(key, val);
+  }
+  if (typeof children === "string") {
+    el.textContent = children;
+  } else if (Array.isArray(children)) {
+    mountChildren(children, el);
+  }
+  container.append(el);
+}
+
+function mountChildren(children, container) {
+  children.forEach((item) => {
+    patch(item, container);
+  });
 }
 
 function processComponent(vnode, container) {
@@ -20,7 +51,7 @@ function mountComponent(vnode, container) {
 }
 function setupRenderEffect(instance, container) {
   const subTree = instance.render();
-  
+
   // vnode => patch
   // vnode => element -> mountElement
   patch(subTree, container);
