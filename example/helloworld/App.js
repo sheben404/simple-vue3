8ed54f7b3beefe182e3b4dcaba82b475d40a4f6c
sheben404 <sheben404@gmail.com>
2022-03-07 10:07:39 +0800

    feat: rollup 打包相关配置

--- example/helloworld/App.js
+++ example/helloworld/App.js
@@ -1,6 +1,13 @@
+import { h } from "../../lib/mini-vue.esm.js";
+
 export const App = {
   render() {
     // ui
-    return h
-  }
-}
+    return h("div", "hi, " + this.msg);
+  },
+  setup() {
+    return {
+      msg: "mini-vue",
+    };
+  },
+};
