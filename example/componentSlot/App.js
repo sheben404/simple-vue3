4d44041bdd464329c47fa08537f234f61de54211
sheben404 <sheben404@gmail.com>
2022-03-08 10:19:48 +0800

    feat: 实现组件 slots 功能

---
+++ example/componentSlot/App.js
@@ -0,0 +1,30 @@
+import { h } from "../../lib/mini-vue.esm.js";
+import { Foo } from "./Foo.js";
+
+export const App = {
+  render() {
+    const app = h("div", {}, "App");
+    // 单个
+    // const foo = h(Foo, {}, h("p", {}, "123"));
+    //数组
+    // const foo = h(Foo, {}, [h("p", {}, "123"), h("p", {}, "456")]);
+    // 对象
+    // const foo = h(
+    //   Foo,
+    //   {},
+    //   { header: h("p", {}, "header"), footer: h("p", {}, "footer") }
+    // );
+    const foo = h(
+      Foo,
+      {},
+      {
+        header: ({age}) => h("p", {}, "header" + age),
+        footer: () => h("p", {}, "footer"),
+      }
+    );
+    return h("div", {}, [app, foo]);
+  },
+  setup() {
+    return {};
+  },
+};
