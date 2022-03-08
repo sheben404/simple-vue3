f4c9222d2bc99d7d453d685713b8a22aedf621a3
sheben404 <sheben404@gmail.com>
2022-03-08 14:16:20 +0800

    feat: 实现 getCurrentInstance

--- src/runtime-core/component.ts
+++ src/runtime-core/component.ts
@@ -31,10 +31,13 @@ function setupStatefulComponent(instance) {
 
   const { setup } = Component;
   if (setup) {
+    setCurrentInstance(instance);
     // function or object
     const setupResult = setup(shallowReadonly(instance.props), {
       emit: instance.emit,
     });
+
+    setCurrentInstance(null);
 
     handleSetupResult(instance, setupResult);
   }
@@ -49,4 +52,17 @@ function handleSetupResult(instance, setupResult) {
 function finishComponentSetup(instance) {
   const Component = instance.type;
   instance.render = Component.render;
+}
+
+let currentInstance = null;
+
+export function getCurrentInstance() {
+  return currentInstance;
+}
+
+// 抽离出函数，有助于我们后续想要跟踪 currentInstance 时
+// 只需要在 setCurrentInstance 这个函数中打断点
+// 然后调试即可
+export function setCurrentInstance(instance) {
+  currentInstance = instance;
 }
