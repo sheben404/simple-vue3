aa944cb36885cbb5b8305a6a555f453189094c01
sheben404 <sheben404@gmail.com>
2022-03-08 11:14:25 +0800

    feat: 实现创建 Fragment 和 Text 类型节点

--- example/componentSlot/App.js
+++ example/componentSlot/App.js
@@ -1,4 +1,4 @@
-import { h } from "../../lib/mini-vue.esm.js";
+import { createTextVNode, h } from "../../lib/mini-vue.esm.js";
 import { Foo } from "./Foo.js";
 
 export const App = {
@@ -18,7 +18,10 @@ export const App = {
       Foo,
       {},
       {
-        header: ({age}) => h("p", {}, "header" + age),
+        header: ({ age }) => [
+          h("p", {}, "header" + age),
+          createTextVNode("你好"),
+        ],
         footer: () => h("p", {}, "footer"),
       }
     );
--- src/runtime-core/render.ts
+++ src/runtime-core/render.ts
@@ -1,18 +1,38 @@
 import { getEventName, isOnEvent } from "../shared/index";
 import { ShapeFlags } from "../shared/ShapeFlags";
 import { createComponentInstance, setupComponent } from "./component";
+import { Fragment, Text } from "./vnode";
 
 export function render(vnode, container) {
   patch(vnode, container);
 }
 function patch(vnode, container) {
-  const { shapeFlag } = vnode;
-  if (shapeFlag & ShapeFlags.ELEMENT) {
-    processElement(vnode, container);
-  } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
-    // 处理组件
-    processComponent(vnode, container);
+  const { type, shapeFlag } = vnode;
+  switch (type) {
+    case Fragment:
+      processFragment(vnode, container);
+      break;
+    case Text:
+      processText(vnode, container);
+      break;
+    default:
+      if (shapeFlag & ShapeFlags.ELEMENT) {
+        processElement(vnode, container);
+      } else if (shapeFlag & ShapeFlags.STATEFUL_COMPONENT) {
+        // 处理组件
+        processComponent(vnode, container);
+      }
+      break;
   }
+}
+
+function processFragment(vnode, container) {
+  mountChildren(vnode, container);
+}
+function processText(vnode, container) {
+  const { children } = vnode;
+  const textNode = (vnode.el = document.createTextNode(children));
+  container.append(textNode);
 }
 
 function processElement(vnode, container) {
@@ -33,13 +53,13 @@ function mountElement(vnode, container) {
   if (shapeFlag & ShapeFlags.TEXT_CHILDREN) {
     el.textContent = children;
   } else if (shapeFlag & ShapeFlags.ARRAY_CHILDREN) {
-    mountChildren(children, el);
+    mountChildren(vnode, el);
   }
   container.append(el);
 }
 
-function mountChildren(children, container) {
-  children.forEach((item) => {
+function mountChildren(vnode, container) {
+  vnode.children.forEach((item) => {
     patch(item, container);
   });
 }
